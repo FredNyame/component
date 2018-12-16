29c9b1a2c15f7fef270f4678cf49b0396f739739
FredNyame <fnyame63@gmail.com>
2018-12-16 00:20:44 -0500

    Files added to the repo

---
+++ main.js
@@ -0,0 +1,21 @@
+//Get Mobile Menu
+let mobileBtn = document.querySelector('.mobile_nav');
+mobileBtn.addEventListener('click', animateBtn);
+function animateBtn(){
+  //Get the bar menu
+  let menuBtn = document.querySelector('#nav');
+  if(mobileBtn.className === 'mobile_nav'){
+     mobileBtn.classList.add('change');
+     //show menu
+      menuBtn.style.visibility = 'visible';
+      menuBtn.style.width = '300px';
+      document.body.style.marginRight = '300px';
+
+  } else{
+    mobileBtn.classList.remove('change');
+    //hide menu
+    menuBtn.style.visibility = 'hidden';
+      menuBtn.style.width = '0px';
+      document.body.style.marginRight = '0px';
+  }
+}
