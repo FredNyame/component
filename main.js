5b37a48af15ee18f3a587d63f8e65bfba22d4183
FredNyame <fnyame63@gmail.com>
2018-12-18 01:59:29 -0500

    CSS and JS changes

--- main.js
+++ main.js
@@ -21,3 +21,64 @@ function animateBtn(){
       //document.body.style.marginRight = '0px';
   }
 }
+
+//Accordion
+let btnShow = document.getElementsByClassName('accordion');
+let btnShowLen = btnShow.length;
+//loop through and add event listener
+for (let i = 0; i < btnShowLen; i++) {
+  btnShow[i].addEventListener('click', showPanel);
+}
+//Show Panel Function
+function showPanel(e){
+  this.classList.toggle('active');
+  let panel = e.target.nextElementSibling;
+  //check if panel has height
+  if(panel.style.maxHeight){
+    panel.style.maxHeight = null;
+  } else{
+    panel.style.maxHeight = panel.scrollHeight+'px';
+  }
+  
+}
+
+//Testimonial Carousel
+//Addevent to next
+let next = document.querySelector('.next');
+next.addEventListener('click', function(){
+  controlSlides(1)
+});
+//Addevent to prev
+let prev = document.querySelector('.prev');
+prev.addEventListener('click', function(){
+  controlSlides(-1);
+});
+
+//Keep track of the currentSlide
+let countSlide = 1;
+function controlSlides(n){
+  toShowCard(countSlide += n);
+}
+
+//Main function
+function toShowCard(n){
+  //hide all the card and only show one
+  let card = document.getElementsByClassName('testimonial');
+  let cardLen = card.length;
+  //Track slide
+  if(n > cardLen){
+    countSlide = 1;
+  }
+  if(n < 1){
+    countSlide = cardLen;
+  }
+  //loop them
+  for (let i = 0; i < cardLen; i++) {
+    //Kepep everything display to none 
+    card[i].style.display = 'none';
+  }
+  //At least one card showing
+  card[countSlide - 1].style.display = 'block';
+}
+toShowCard();
+
